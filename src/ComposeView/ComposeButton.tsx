5dbdf5562271f04271ce5c64c91a503caa6f9b1a
Vince Cooley <vince.r.cooley@gmail.com>
2024-08-21 19:37:28 -0600

    render the compose button with a context to the button view and element ref

--- src/ComposeView/ComposeButton.tsx
+++ src/ComposeView/ComposeButton.tsx
@@ -1,5 +1,4 @@
-import { createContext, useEffect, useRef } from "react";
-import { createPortal } from "react-dom";
+import { Ref, createContext, useMemo, useRef } from "react";
 import { ComposeButtonDescriptor } from "@inboxsdk/core";
 import ComposeButtonView from "@inboxsdk/core/src/platform-implementation-js/views/compose-button-view";
 
@@ -10,7 +9,15 @@ type ComposeButtonProps = ComposeButtonDescriptor & {
   children: React.ReactNode;
 };
 
-const ComposeButtonContext = createContext<ComposeButtonView | null>(null);
+type ComposeButtonContextValue = {
+  buttonRef: Ref<HTMLElement> | null;
+  composeView: ComposeButtonView | null;
+};
+
+const ComposeButtonContext = createContext<ComposeButtonContextValue>({
+  buttonRef: null,
+  composeView: null,
+});
 
 function createClassHash() {
   return "inbox-react-" + makeHash(8);
@@ -19,10 +26,14 @@ function createClassHash() {
 function ComposeButton(props: ComposeButtonProps) {
   const composeView = useComposeView();
   const composeButtonRef = useRef<ComposeButtonView | null>(null);
-  const composeButtonNode = useRef<HTMLElement | null>(null);
-  const { children, iconClass, ...composeButtonDescriptor } = props;
+  const composeButtonNodeRef = useRef<HTMLElement | null>(null);
+  const didInit = useRef<boolean>(false);
 
-  useEffect(() => {
+  const { children, ...composeButtonDescriptor } = props;
+
+  if (!didInit.current) {
+    didInit.current = true;
+
     if (!composeView) {
       console.error("ComposeButton must be wrapped in a ComposeView.");
       return;
@@ -30,19 +41,32 @@ function ComposeButton(props: ComposeButtonProps) {
 
     // InboxSDK doesn't expose a handle to the HTML element backing the button, so we need to
     // create a unique class name and query the DOM for it
+    const { iconClass } = composeButtonDescriptor;
     const classHash = createClassHash();
     const className = iconClass ? classHash + " " + iconClass : classHash;
+
     composeButtonRef.current = composeView.addButton({
       ...composeButtonDescriptor,
       iconClass: className,
     });
-    composeButtonNode.current = document.querySelector(`.${className}`);
-  }, []);
+    composeButtonNodeRef.current = document.querySelector(`.${className}`);
+
+    if (!composeButtonNodeRef.current) {
+      console.error("Couldn't find a compose button to attach to.");
+    }
+  }
+
+  const contextValue = useMemo(
+    () => ({
+      buttonRef: composeButtonNodeRef,
+      composeView: composeButtonRef.current,
+    }),
+    [],
+  );
 
   return (
-    <ComposeButtonContext.Provider value={composeButtonRef.current}>
-      {composeButtonNode.current &&
-        createPortal(children, composeButtonNode.current)}
+    <ComposeButtonContext.Provider value={contextValue}>
+      {children}
     </ComposeButtonContext.Provider>
   );
 }
