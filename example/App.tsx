0359a135c1060e1d8c2d8614f4be43cbbd7ecf7e
Vince Cooley <vince.r.cooley@gmail.com>
2024-08-25 14:38:19 -0600

    update example

--- example/App.tsx
+++ example/App.tsx
@@ -2,19 +2,8 @@ import InboxSDK from "react-inbox-sdk/InboxSDK";
 import ThreadView, {
   NoticeBar,
 } from "react-inbox-sdk/Conversations/ThreadView";
-import { useNoticeBar } from "react-inbox-sdk/Conversations/ThreadView/NoticeBar";
-import { createPortal } from "react-dom";
 
 const INBOX_SDK_APP_ID = process.env.INBOX_SDK_APP_ID ?? "";
-
-function MyNoticeBar() {
-  const noticeBar = useNoticeBar();
-  if (!noticeBar) return null;
-  return createPortal(
-    <div style={{ backgroundColor: "red", color: "white" }}>Hello World!</div>,
-    noticeBar.el,
-  );
-}
 
 function App() {
   console.log("Rendered React App");
@@ -22,7 +11,9 @@ function App() {
     <InboxSDK appId={INBOX_SDK_APP_ID}>
       <ThreadView>
         <NoticeBar>
-          <MyNoticeBar />
+          <div style={{ backgroundColor: "red", color: "white" }}>
+            Hello World!
+          </div>
         </NoticeBar>
       </ThreadView>
     </InboxSDK>
