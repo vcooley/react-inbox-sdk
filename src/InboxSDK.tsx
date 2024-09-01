47c655f4eb39a042970644b5c6c50050b8e58bd4
Vince Cooley <vince.r.cooley@gmail.com>
2024-09-01 16:37:13 -0600

    export an instance of the sdk

--- src/InboxSDK.tsx
+++ src/InboxSDK.tsx
@@ -9,6 +9,11 @@ import {
 
 let sdk: InboxSDKType;
 
+/** Get an instance of the SDK. */
+export function getSdk(): InboxSDKType | undefined {
+  return sdk;
+}
+
 // @ts-expect-error - We're not rendering children until the SDK is loaded
 const InboxSDKContext = createContext<InboxSDKType>(null);
 
@@ -16,20 +21,34 @@ export const useInboxSDK = () => useContext(InboxSDKContext);
 
 export default function InboxSDK({
   appId,
+  instance,
   children,
 }: {
-  appId: string;
-  children: ReactNode;
+  appId?: string;
+  instance?: InboxSDKType;
+  children?: ReactNode;
 }) {
   const [, setRenderTrigger] = useState(0);
   useEffect(() => {
     if (sdk) return;
-    load(2, appId).then((loaded) => {
-      sdk = loaded;
-      // Since we're keeping the SDK globally, we need to trigger a re-render manually
+    if (!appId && !instance) {
+      console.warn(
+        "The InboxSDK React adapter was rendered without an app ID or sdk instance and will not render any of its children until one is provided.",
+      );
+      return;
+    }
+
+    if (instance) {
+      sdk = instance;
       setRenderTrigger((current) => current + 1);
-    });
-  }, [appId]);
+    } else if (appId) {
+      load(2, appId).then((loaded) => {
+        sdk = loaded;
+        // Since we're keeping the SDK globally, we need to trigger a re-render manually
+        setRenderTrigger((current) => current + 1);
+      });
+    }
+  }, [appId, instance]);
 
   if (!sdk) return null;
 
